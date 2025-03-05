2a77d5be128e9587abf6db3babfcb17e1959da59
s9poa <s9poah@gmail.com>
2025-03-05 07:13:18 -0500

    Refined the Design of Tracker Arrows for Game Links

--- javascript/global/tracker-icon.js
+++ javascript/global/tracker-icon.js
@@ -1,14 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const icons = document.querySelectorAll('.section-break__icon');
-    const observerOptions = { root: null, threshold: 0.5 };
-    const observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add('in-view');
-          observer.unobserve(entry.target);
-        }
-      });
-    }, observerOptions);
-    icons.forEach(icon => observer.observe(icon));
-  });
-  
+  const elements = document.querySelectorAll('.section-break__icon, .section-break__icon__dash');
+  const observerOptions = { root: null, threshold: 0.5 };
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('in-view');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, observerOptions);
+  elements.forEach(el => observer.observe(el));
+});
