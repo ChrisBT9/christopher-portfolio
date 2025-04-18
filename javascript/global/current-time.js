22ab6788edf23cbb2c1bc8c84b436fddee684bb8
s9poa <s9poah@gmail.com>
2025-04-17 21:12:53 -0400

    Updated Content

--- javascript/global/current-time.js
+++
@@ -1,9 +0,0 @@
-// Give an element the 'id' of current-time 
-
-const currentTimeElement = document.getElementById("current-time");
-function updateCurrentTime() {
-  const now = new Date();
-  currentTimeElement.textContent = now.toLocaleTimeString([], { hour: "numeric", minute: "numeric", hour12: true });
-}
-updateCurrentTime();
-setInterval(updateCurrentTime, 60000);
